12e03b45b933a28f426688a9f40b142e31a66253
steve806 <steve8060914@gmail.com>
2023-07-20 01:48:24 +0900

    fix: validator address to user1

--- config/multisig_ism.ts
+++ config/multisig_ism.ts
@@ -5,16 +5,16 @@ export const multisigIsmConfig: ChainMap<MultisigIsmConfig> = {
   mitosis: {
     type: ModuleType.LEGACY_MULTISIG,
     threshold: 1,
-    validators: ['0x594D0C5F2ba861214C74314eF4b0927A5E6f0Cb9'],
+    validators: ['0x3a4c9ad12b4daF20ae4635F9c2C0a03af583558a'],
   },
   anvil1: {
     type: ModuleType.LEGACY_MULTISIG,
     threshold: 1,
-    validators: ['0x594D0C5F2ba861214C74314eF4b0927A5E6f0Cb9'],
+    validators: ['0x3a4c9ad12b4daF20ae4635F9c2C0a03af583558a'],
   },
   anvil2: {
     type: ModuleType.LEGACY_MULTISIG,
     threshold: 1,
-    validators: ['0x594D0C5F2ba861214C74314eF4b0927A5E6f0Cb9'],
+    validators: ['0x3a4c9ad12b4daF20ae4635F9c2C0a03af583558a'],
   },
 };
