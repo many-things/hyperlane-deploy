7139590eef1d543450c99f886559a09308bb724a
byeongsu-hong <hong@byeongsu.dev>
2023-07-23 18:32:31 +0000

    feat: add goerli settings

--- config/chains.ts
+++ config/chains.ts
@@ -1,47 +1,85 @@
 import { ChainMap, ChainMetadata, ProtocolType } from '@hyperlane-xyz/sdk';
 
+const {
+  MITOSIS_GOERLI_CHAIN_ID = '26657',
+  MITOSIS_GOERLI_RPC_ENDPOINT = 'http://localhost:8545',
+
+  MITOSIS_LOCALNET_CHAIN_ID = '26657',
+  MITOSIS_LOCALNET_RPC_ENDPOINT = 'http://localhost:8545',
+
+  ANVIL1_CHAIN_ID = '31338',
+  ANVIL1_RPC_ENDPOINT = 'http://localhost:8555',
+
+  ANVIL2_CHAIN_ID = '31339',
+  ANVIL2_RPC_ENDPOINT = 'http://localhost:8565',
+
+  ANVIL3_CHAIN_ID = '31340',
+  ANVIL3_RPC_ENDPOINT = 'http://localhost:8575',
+
+  GOERLI_CHAIN_ID = '5',
+  GOERLI_RPC_ENDPOINT = '',
+
+  ARBITRUM_GOERLI_CHAIN_ID = '5',
+  ARBITRUM_GOERLI_RPC_ENDPOINT = '',
+
+  OPTIMISM_GOERLI_CHAIN_ID = '5',
+  OPTIMISM_GOERLI_RPC_ENDPOINT = '',
+} = process.env;
+
 // import { chainMetadata } from '@hyperlane-xyz/sdk';
 // A map of chain names to ChainMetadata
 export const chains: ChainMap<ChainMetadata> = {
-  // ----------- Add your chains here -----------------
-  mitosis: {
-    name: 'mitosis',
+  // mitosis
+  'mitosis-goerli': {
+    name: 'mitosis-goerli',
     protocol: ProtocolType.Ethereum,
-    chainId: 26657,
-    rpcUrls: [
-      {
-        http: 'http://127.0.0.1:8545',
-      },
-    ],
+    chainId: Number(MITOSIS_GOERLI_CHAIN_ID),
+    rpcUrls: [{ http: MITOSIS_GOERLI_RPC_ENDPOINT }],
   },
+  'mitosis-localnet': {
+    name: 'mitosis-localnet',
+    protocol: ProtocolType.Ethereum,
+    chainId: Number(MITOSIS_LOCALNET_CHAIN_ID),
+    rpcUrls: [{ http: MITOSIS_LOCALNET_RPC_ENDPOINT }],
+  },
+
+  // localnet settings
   anvil1: {
     name: 'anvil1',
     protocol: ProtocolType.Ethereum,
-    chainId: 31338,
-    rpcUrls: [
-      {
-        http: 'http://127.0.0.1:8555',
-      },
-    ],
+    chainId: Number(ANVIL1_CHAIN_ID),
+    rpcUrls: [{ http: ANVIL1_RPC_ENDPOINT }],
   },
   anvil2: {
     name: 'anvil2',
     protocol: ProtocolType.Ethereum,
-    chainId: 31339,
-    rpcUrls: [
-      {
-        http: 'http://127.0.0.1:8565',
-      },
-    ],
+    chainId: Number(ANVIL2_CHAIN_ID),
+    rpcUrls: [{ http: ANVIL2_RPC_ENDPOINT }],
   },
   anvil3: {
     name: 'anvil3',
     protocol: ProtocolType.Ethereum,
-    chainId: 31340,
-    rpcUrls: [
-      {
-        http: 'http://127.0.0.1:8575',
-      },
-    ],
+    chainId: Number(ANVIL3_CHAIN_ID),
+    rpcUrls: [{ http: ANVIL3_RPC_ENDPOINT }],
+  },
+
+  // testnet settings
+  goerli: {
+    name: 'goerli',
+    protocol: ProtocolType.Ethereum,
+    chainId: Number(GOERLI_CHAIN_ID),
+    rpcUrls: [{ http: GOERLI_RPC_ENDPOINT }],
+  },
+  arbitrumgoerli: {
+    name: 'arbitrum-goerli',
+    protocol: ProtocolType.Ethereum,
+    chainId: Number(ARBITRUM_GOERLI_CHAIN_ID),
+    rpcUrls: [{ http: ARBITRUM_GOERLI_RPC_ENDPOINT }],
+  },
+  optimismgoerli: {
+    name: 'optimism-goerli',
+    protocol: ProtocolType.Ethereum,
+    chainId: Number(OPTIMISM_GOERLI_CHAIN_ID),
+    rpcUrls: [{ http: OPTIMISM_GOERLI_RPC_ENDPOINT }],
   },
 };
--- config/multisig_ism.ts
+++ config/multisig_ism.ts
@@ -1,25 +1,52 @@
 import { ChainMap, ModuleType, MultisigIsmConfig } from '@hyperlane-xyz/sdk';
 
+const {
+  HPL_GOERLI_VALIDATOR = '0x3a4c9ad12b4daF20ae4635F9c2C0a03af583558a',
+  HPL_LOCALNET_VALIDATOR = '0x3a4c9ad12b4daF20ae4635F9c2C0a03af583558a',
+} = process.env;
+
 export const multisigIsmConfig: ChainMap<MultisigIsmConfig> = {
-  // ----------- Your chains here -----------------
-  mitosis: {
+  // localnet settings
+  'mitosis-localnet': {
     type: ModuleType.LEGACY_MULTISIG,
     threshold: 1,
-    validators: ['0x3a4c9ad12b4daF20ae4635F9c2C0a03af583558a'],
+    validators: [HPL_LOCALNET_VALIDATOR],
   },
   anvil1: {
     type: ModuleType.LEGACY_MULTISIG,
     threshold: 1,
-    validators: ['0x3a4c9ad12b4daF20ae4635F9c2C0a03af583558a'],
+    validators: [HPL_LOCALNET_VALIDATOR],
   },
   anvil2: {
     type: ModuleType.LEGACY_MULTISIG,
     threshold: 1,
-    validators: ['0x3a4c9ad12b4daF20ae4635F9c2C0a03af583558a'],
+    validators: [HPL_LOCALNET_VALIDATOR],
   },
   anvil3: {
     type: ModuleType.LEGACY_MULTISIG,
     threshold: 1,
-    validators: ['0x3a4c9ad12b4daF20ae4635F9c2C0a03af583558a'],
+    validators: [HPL_LOCALNET_VALIDATOR],
+  },
+
+  // goerli settings
+  'mitosis-goerli': {
+    type: ModuleType.LEGACY_MULTISIG,
+    threshold: 1,
+    validators: [HPL_GOERLI_VALIDATOR],
+  },
+  goerli: {
+    type: ModuleType.LEGACY_MULTISIG,
+    threshold: 1,
+    validators: [HPL_GOERLI_VALIDATOR],
+  },
+  arbitrumgoerli: {
+    type: ModuleType.LEGACY_MULTISIG,
+    threshold: 1,
+    validators: [HPL_GOERLI_VALIDATOR],
+  },
+  optimismgoerli: {
+    type: ModuleType.LEGACY_MULTISIG,
+    threshold: 1,
+    validators: [HPL_GOERLI_VALIDATOR],
   },
 };
